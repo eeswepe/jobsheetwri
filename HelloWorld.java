542716da2492f11243521855212651773cf7cef4
eeswepe <singgihwahyupermana@gmail.com>
2024-09-03 20:53:31 +0700

    second commit via vscode

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.err.println("Pak vincent said: ");
+        for(int i=2;i< 1000; i++){
+            System.err.println("saya satu sama itu saya " + i);
+        }
     }
 
 }
